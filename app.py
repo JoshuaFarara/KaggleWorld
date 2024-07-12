a8a7990c36cce181feb0153c2870baf4a4d0d276
JoshuaFarara <joshua.farara@gmail.com>
2024-07-11 22:14:15 -0400

    Changes to be committed: 	modified:   app.py 	modified:   jupyter_notebook_template.ipynb 	modified:   templates/index.html 	new file:   templates/navigation.html

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route('/')
+def home():
     # return "<p>Hello, World!</p>"
     name = "Joshua Farara"
     age = 28
@@ -12,3 +12,7 @@ def hello_world():
     masters =  "MS in Computer Science"
     
     return render_template('index.html', name=name, age=age, occupation=occupation, undergrad=undergrad, masters=masters)
+
+@app.route('/data-analysis')
+def data_analysis():
+    pass
