ba85f7de76d8f8354023d0d74a9c123a410be617
JoshuaFarara <joshua.farara@gmail.com>
2024-07-13 16:16:16 -0400

    Wrote script to generate new jupyter notebooks :
    	deleted:    coffee_sales_analyisis.ipynb
    	new file:   notebooks/Testing Structure.ipynb
    	new file:   notebooks/__pycache__/notebook_template.cpython-311.pyc
    	renamed:    amazon-sales-data-science.ipynb -> notebooks/amazon-sales-data-science.ipynb
    	new file:   notebooks/coffee_sales_analyisis.ipynb
    	renamed:    jupyter_notebook_template.ipynb -> notebooks/notebook_templates/jupyter_notebook_template.ipynb
    	new file:   notebooks/scripts/__pycache__/notebook_template.cpython-311.pyc
    	new file:   notebooks/scripts/new_notebook.py
    	new file:   notebooks/scripts/notebook_template.py
    	renamed:    traffic-data-analysis.ipynb -> notebooks/traffic-data-analysis.ipynb
    
    Changes not staged for commit:
    	deleted:    notebooks/Testing Structure.ipynb

---
+++ notebooks/scripts/new_notebook.py
@@ -0,0 +1,44 @@
+'''
+Generate a template Jupyter Notebook. 
+
+user input of title
+
+'''
+
+import os
+import json
+from notebook_template import get_notebook_structure
+
+
+class Notebook:
+
+    def __init__(self, notebook_title):
+        self.notebook_title = notebook_title
+        self.folder_path  = "./notebooks"
+        if not os.path.exists(self.folder_path):
+            os.makedirs(self.folder_path)
+
+
+    def generate_notebook(self):
+        return get_notebook_structure(self.notebook_title)
+
+    def get_title(self):
+        print(f"Notebook title: {self.notebook_title}")
+
+    def save_template(self):
+        notebook_content = self.generate_notebook()
+
+        sanitized_title = self.notebook_title.lower().replace(' ', '_')
+        
+        # Create the file path
+        filepath = os.path.join(self.folder_path, f"{sanitized_title}.ipynb")
+        
+        # Write the notebook content to the file
+        with open(filepath, 'w') as fp:
+            json.dump(notebook_content, fp, indent=4)
+        print(f"Notebook '{self.notebook_title}' has been created at {filepath}")
+
+title = input('Enter a title of the notebook: ')
+notebook = Notebook(title)
+notebook.get_title()
+notebook.save_template()
