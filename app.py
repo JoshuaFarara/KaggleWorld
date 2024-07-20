5f27fbdb5a1359515d6c0506639723777b856b46
JoshuaFarara <joshua.farara@gmail.com>
2024-07-20 00:29:43 -0400

    Started Notebooks Dashboard, sidebar, css: 	modified:   __pycache__/app.cpython-311.pyc 	modified:   app.py 	modified:   drafting.txt 	new file:   notebooks/cofee_sales/coffee_sales_analyisis.html 	new file:   static/styles.css 	modified:   templates/base.html 	modified:   templates/index.html 	modified:   templates/navigation.html 	new file:   templates/notebook-dashboard.html 	new file:   templates/template.html

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    
-    return render_template('base.html')
+@app.route('/home')
+def home(): 
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
@@ -15,9 +16,10 @@ def about():
     occupation = "Visiting Instructor of Computer Science at Georgia Southern University"
     undergrad = "BS in Economics"
     masters =  "MS in Computer Science"
-    
     return render_template('about.html', name=name, age=age, occupation=occupation, undergrad=undergrad, masters=masters)
 
-@app.route('/data-analysis')
-def data_analysis():
-    pass
+@app.route('/notebook-dashboard')
+def notebooks():
+    notebooks_folder = './notebooks'
+    notebooks = os.listdir(notebooks_folder)
+    return render_template('notebook-dashboard.html', notebooks=notebooks)
