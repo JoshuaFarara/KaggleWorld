11545e97c060221271f85b15ceed0450a0f58082
JoshuaFarara <joshua.farara@gmail.com>
2024-08-02 15:55:57 -0400

    Streamlit successfully embedded in dashboard view: 	modified:   __pycache__/app.cpython-311.pyc 	modified:   app.py 	new file:   coffee_sales_streamlit.py 	modified:   drafting.txt 	modified:   static/styles.css 	modified:   templates/cofee_sales.html 	modified:   templates/notebook-dashboard.html

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, render_template
 import os
+import subprocess
+import threading
+
 
 app = Flask(__name__)
 
@@ -18,15 +21,32 @@ def about():
     masters =  "MS in Computer Science"
     return render_template('about.html', name=name, age=age, occupation=occupation, undergrad=undergrad, masters=masters)
 
+'''Base notebook dashboard navigation'''
 @app.route('/notebook-dashboard')
 def notebooks():
+    # Start the Streamlit app if it's not already running
+    # if 'STREAMLIT_SERVER' not in os.environ:
+    #     os.environ['STREAMLIT_SERVER'] = '1'
+    #     subprocess.Popen(["streamlit", "run", "streamlit_app.py"])
+
     notebooks_folder = './notebooks'
     notebooks = os.listdir(notebooks_folder)
     return render_template('notebook-dashboard.html', notebooks=notebooks)
 
+'''Display data within the notebook dashboard'''
 @app.route('/cofee_sales')
 def cofee_sales():
+    def run_streamlit():
+        if 'STREAMLIT_SERVER' not in os.environ:
+            os.environ['STREAMLIT_SERVER'] = '1'
+            subprocess.Popen(["streamlit", "run", "coffee_sales_streamlit.py"])
+        # subprocess.run(["streamlit", "run", "coffee_sales_streamlit.py"])
+
+    thread = threading.Thread(target=run_streamlit)
+    thread.start()
     return render_template('cofee_sales.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
